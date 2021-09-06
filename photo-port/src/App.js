12e245d9be6c8aac86ae1cb05a4e52008eb6c09b
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-06 12:26:40 -0400

    src App.js

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "components/Header";
+import About from './components/About';
+import ContactForm from './components/Contact';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Footer from './components/Footer';
 
-function App() {
+
+function App () {
+  const [currentTab, SetCurrentTab] = useState('About');
+  const renderPage = () => {
+    switch (currentTab) {
+      case 'About' : return <About />;
+      case 'Portfolio' : return <Portfolio />;
+      case 'Contact' : return <Contact />;
+      case 'Résumé' : return <Resume />;
+      default : return <About />;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header currentTab={currentTab} SetCurrentTab={SetCurrentTab} />
+      <div className="mb-5">
+        <main className='d-flex justify-content-around text-center'>{renderPage()}</main>
+      </div>
+      <Footer />
     </div>
   );
 }
