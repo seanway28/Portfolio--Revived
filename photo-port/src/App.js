b505f5d0473b896eb1ed732b836e15402f7bb56b
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-20 00:10:06 -0400

    app.js start

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,35 +1,65 @@
 import React, { useState } from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from "components/Header";
-import About from './components/About';
-import ContactForm from './components/Contact';
-import Resume from './components/Resume';
-import Portfolio from './components/Portfolio';
-import Footer from './components/Footer';
+
+import { validateEmail } from './utils/helpers';
+
+import github from './assets/css/github.svg';
+import linkedin from './assets/css/linkedin.svg';
+import instagram from './assets/css/instagram.svg';
 
 
-function App () {
-  const [currentTab, SetCurrentTab] = useState('About');
-  const renderPage = () => {
-    switch (currentTab) {
-      case 'About' : return <About />;
-      case 'Portfolio' : return <Portfolio />;
-      case 'Contact' : return <Contact />;
-      case 'Résumé' : return <Resume />;
-      default : return <About />;
-    }
-  };
 
-  return (
-    <div className="App">
-      <Header currentTab={currentTab} SetCurrentTab={SetCurrentTab} />
-      <div className="mb-5">
-        <main className='d-flex justify-content-around text-center'>{renderPage()}</main>
-      </div>
-      <Footer />
-    </div>
-  );
-}
 
-export default App;
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import './App.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Header from "components/Header";
+// import About from './components/About';
+// import ContactForm from './components/Contact';
+// import Resume from './components/Resume';
+// import Portfolio from './components/Portfolio';
+// import Footer from './components/Footer';
+
+
+// function App () {
+//   const [currentTab, SetCurrentTab] = useState('About');
+//   const renderPage = () => {
+//     switch (currentTab) {
+//       case 'About' : return <About />;
+//       case 'Portfolio' : return <Portfolio />;
+//       case 'Contact' : return <Contact />;
+//       case 'Résumé' : return <Resume />;
+//       default : return <About />;
+//     }
+//   };
+
+//   return (
+//     <div className="App">
+//       <Header currentTab={currentTab} SetCurrentTab={SetCurrentTab} />
+//       <div className="mb-5">
+//         <main className='d-flex justify-content-around text-center'>{renderPage()}</main>
+//       </div>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
