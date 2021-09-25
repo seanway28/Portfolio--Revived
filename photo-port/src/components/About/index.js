cfd9684db97fbe6c7219034298868eee9df7df0a
Sean Conway <81600120+seanway28@users.noreply.github.com>
2021-09-25 13:02:45 -0400

    update App js

--- photo-port/src/components/About/index.js
+++ photo-port/src/components/About/index.js
@@ -10,8 +10,8 @@ const About = () => {
   <div className="body-text"/>
   <h3 className="title-text">Sean M. Conway</h3>
           <p>
-          Hello, I’m @seanway28, but you can call me Sean!
-        I’m interested in full stack development, mobile app development, cyber security and other new opportunities. I believe I can combine this new knowledge with by previous 10+ years as a Project Manager for several Caterpillar dealerships and corporate offices and my time at General Electric that gave me the inspiration to start my coding studies.
+         
+        Butler University Full Stack Code Development Executive Bootcamp recent  , mobile app development, cyber security and other new opportunities. I believe I can combine this new knowledge with by previous 10+ years as a Project Manager for several Caterpillar dealerships and corporate offices and my time at General Electric that gave me the inspiration to start my coding studies.
         I’m currently a student in the Butler University Full Stack Code Development Executive Bootcamp Program!
         I’m looking to work and collaborate anywhere that encourages growth and expansion in both knowledge and practice. I bring leadership and well over a decade of electrical engineering and project management experience from both Caterpillar and General Electric. I look forward using my skillsets, both new and old, in today's ever growing technical market.
         The best way to reach me is at the following email address seanway28@gmail.com
